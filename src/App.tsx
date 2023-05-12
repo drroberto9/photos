69e7966fe7e17388a96a899bc80d7f078f48fad9
rdelrosariojr <roberto.delrosario@sun-asterisk.com>
2023-05-13 00:35:15 +0800

    finished sample project

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,75 @@
-import React from "react";
-import logo from "./logo.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 const App = () => {
-  return <div className="App">Hello World</div>;
+  const [photos, setPhotos] = useState<string[]>([""]);
+  const [currentPhotoID, setCurrentPhotoID] = useState(0);
+  const [photoSize, setPhotoSize] = useState(400);
+
+  const getPhoto = async () => {
+    try {
+      const response = await axios.get(
+        "https://api.pexels.com/v1/search?query=food",
+        {
+          headers: {
+            Authorization:
+              "DxDDurrVL9ZLlepBOwTmqz9CDzVzaU0S78o1t8ln27IYuggN5t3hRT0R",
+          },
+        }
+      );
+
+      setPhotos(response.data.photos.map((photo: any) => photo.src.large));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getPhoto();
+  }, []);
+
+  return (
+    <div className="App">
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100vh",
+        }}
+      >
+        <div>
+          <img
+            src={photos[currentPhotoID]}
+            style={{
+              width: photoSize,
+              height: photoSize,
+              borderRadius: photoSize / 5,
+            }}
+          />
+          <button
+            className="btn"
+            onClick={() => {
+              if (currentPhotoID !== 14) setCurrentPhotoID(currentPhotoID + 1);
+              else setCurrentPhotoID(0);
+            }}
+          >
+            Change
+          </button>
+          <button
+            className="btn-2"
+            onClick={() => {
+              if (photoSize !== 700) setPhotoSize(photoSize + 50);
+              else setPhotoSize(400);
+            }}
+          >
+            Zoom
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
